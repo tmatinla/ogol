310202c3ba25045dce3167ff6229bbad6d79b03a
Tero Matinlassi <terom@iki.fi>
2013-09-30 01:08:17 +0300

    Slowly getting there. Now I'm able to run sketches, but can't stop them! :)

--- src/cljs/sketch.cljs
+++ src/cljs/sketch.cljs
@@ -1,7 +1,24 @@
 (ns ogol.sketch
   (:use [jayq.core :only [$]])
-  (:require [libre.sketch :as sk])
+  (:require [libre.sketch :as sk]
+            [himera.client.repl :as himera])
   (:require-macros [jayq.macros :as jayq]))
+
+(defn- get-textarea-value [textarea]
+  (.-value (.get ($ textarea) 0)))
+
+(defn run-sketch [canvas sketch]
+    (let [input (.trim js/jQuery sketch)
+          compiled (himera/go-compile input)]
+      (if-let [err (and compiled (:error compiled))]
+        (js/alert "Compilation error: " err)
+        (try
+          (sk/run-sketch canvas (js/eval (:js compiled)))
+          (catch js/Error e
+            (js/alert "Compilation error: " e))))))
+
+  ; (js/alert (himera/go-compile sketch)))
+  ;(sk/run-sketch canvas (himera/go-compile sketch)))
 
 (defn ^:export createSketch []
   (jayq/ready
@@ -11,12 +28,7 @@
       (.draggable))
     (doto ($ :#run-sketch)
       (-> (.button (clj->js {:icons {:primary "ui-icon-play"}}))
-      	  (.click (fn [e] (js/ogol.sketch.runSketch)))))
+      	  (.click (fn [e] (run-sketch :#canvas1 (get-textarea-value :#sketch1))))))
     (doto ($ :#stop-sketch)
       (.button (clj->js {:icons {:primary "ui-icon-stop"}})))))
 
-(defn- get-textarea-value [textarea]
-  (.-value (.get ($ textarea) 0)))
-
-(defn ^:export runSketch []
-  (sk/run-sketch :#canvas1 (get-textarea-value :#sketch1)))
--- src/clojure/ogol/handler.clj
+++ src/clojure/ogol/handler.clj
@@ -5,17 +5,25 @@
             [compojure.handler :as handler]
             [compojure.route :as route]
             [himera.server.cljs :as cljs]
-            [himera.server.service :as himera]))
+            [himera.server.service :as himera]
+            [cljs.compiler :as comp]
+            [cljs.analyzer :as ana]
+            [himera.server.setup :as setup]
+            [ogol.setup :as ogolsetup]))
+
+(def compilation (partial cljs/build
+                          #(comp/emit-str (ana/analyze % %2))
+                          (merge (setup/load-core-names) (ogolsetup/load-sk-names))))
 
 (defroutes app-routes
   (GET "/" [] (resp/file-response "index.html" {:root "resources/public"}))
 
   ; Himera service
   (POST "/compile" [expr]
-  	(himera/generate-js-response (cljs/compilation expr :simple false)))
+  	(himera/generate-js-response (compilation expr :simple false)))
   ; not sure if this is needed?
-  (POST "/ast" [expr]
-    (himera/generate-ast-response (cljs/analyze expr :simple true)))
+  ; (POST "/ast" [expr]
+  ;   (himera/generate-ast-response (cljs/analyze expr :simple true)))
 
 
   (route/resources "/")
